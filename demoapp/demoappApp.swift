7d5e2da35cee541e9026be74f77523f97f79b497
Suhail Kakar <suhail.zahed@gmail.com>
2024-01-09 01:52:01 +0530

    Initial Commit

---
+++ demoapp/demoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoappApp.swift
+//  demoapp
+//
+//  Created by Suhail on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct demoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
